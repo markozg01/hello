18592323a5c6a71371b6d4b770f6e16317674af8
marko <marko.klaric@btb.hr>
2016-11-04 09:35:37 +0100

    komit 1

--- Test/src/CaTemp2.java
+++ Test/src/CaTemp2.java
@@ -1,6 +1,6 @@
 public class CaTemp2 {
 
-    private String abcdef4441novoNovoGrana6644333user24441;
+    private String abcdef4441novoNovoGrana6644333user24441333;
 
     private String dodanoMaster123333xx4445;
 
