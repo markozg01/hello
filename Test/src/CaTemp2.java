4629d23b2ae184968a5edf208913b8051e7468e2
marko <marko.klaric@btb.hr>
2016-11-04 09:51:27 +0100

    test user 3

--- Test/src/CaTemp2.java
+++ Test/src/CaTemp2.java
@@ -2,6 +2,6 @@ public class CaTemp2 {
 
     private String abcdef4441novoNovoGrana6644333user24441333;
 
-    private String dodanoMaster123333xx4445222444;
+    private String dodanoMaster123333xx444522244433;
 
 }
