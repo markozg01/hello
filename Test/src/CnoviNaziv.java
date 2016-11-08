a3267a252273219feb7a381698e7a1267bacf2dd
marko <marko.klaric@btb.hr>
2016-11-08 16:01:55 +0100

    komit hello_2

--- Test/src/CnoviNaziv.java
+++ Test/src/CnoviNaziv.java
@@ -2,7 +2,7 @@ public class CnoviNaziv {
 
     private String abcdef4444444444444334444;
 
-    private String abcd444444444444444443eee;
+    private String abcd444444444444444443eee444;
 
     private String abcd44444444444444444444433444444;
 
