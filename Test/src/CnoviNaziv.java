38e8eb97e28b931488163e41aa432b23bb0fbc25
marko <marko.klaric@btb.hr>
2016-11-16 08:18:58 +0100

    4444

--- Test/src/CnoviNaziv.java
+++ Test/src/CnoviNaziv.java
@@ -2,7 +2,7 @@ public class CnoviNaziv {
 
     private String abcdef444444444444433444433;
 
-    private String abcd444444444444444443eee444;
+    private String abcd444444444444444443eee444444;
 
     private String abcd44444444444444444444433444444;
 
